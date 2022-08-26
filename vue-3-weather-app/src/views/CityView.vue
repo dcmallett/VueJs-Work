4e7d5cde1326710c778ccaa3f4e7a165f466c578
dcmallett <dcmallett28@gmail.com>
2022-08-26 23:57:05 +0100

    Added new components and getting data from API

--- vue-3-weather-app/src/views/CityView.vue
+++ vue-3-weather-app/src/views/CityView.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
-
+        <Suspense>
+             <AsyncCityView />
+             <!-- template for our fallback content in case the main component fails for some reason to load the data -->
+             <template #fallback>
+                <p>Loading...</p>
+             </template>
+        </Suspense>
+       
     </div>
 </template>
 
 <script setup>
-
+import AsyncCityView from '../components/AsyncCityView/AsyncCityView.vue';
 </script>
