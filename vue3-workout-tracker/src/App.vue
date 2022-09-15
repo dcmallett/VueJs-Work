ba4db40423d6a21dc130ff2b03ea5dc60a17e9c3
dcmallett <dcmallett28@gmail.com>
2022-09-15 15:22:30 +0100

    Starter files and folders

---
+++ vue3-workout-tracker/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="min-h-full font-Poppins box-border">
+    <TheNavigation />
+    <router-view></router-view>
+  </main>
+</template>
+
+<script setup>
+import TheNavigation from './components/layout/TheNavigation.vue';
+</script>
---
+++ vue3-workout-tracker/src/components/layout/TheNavigation.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <header class="bg-at-light-green text-white">
+            <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
+                <div class="flex items-center gap-x-4">
+                    <img class="w-14" src="../../assets/images/dumbbell-light.png" alt="" />
+                    <h1 class="text-lg">Active Tracker</h1>
+                </div>
+                <ul class="flex flex-1 justify-end gap-x-10">
+                    
+                </ul>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<script setup>
+
+</script>
