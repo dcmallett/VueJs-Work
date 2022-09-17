d177d218f9d910e6d653a05986e5dfa7f9da91f8
dcmallett <dcmallett28@gmail.com>
2022-09-17 14:35:19 +0100

    Added a new login and register form

--- vue3-workout-tracker/src/router/index.js
+++ vue3-workout-tracker/src/router/index.js
@@ -1,13 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/auth/Login.vue';
+import Register from '../views/auth/Register.vue';
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: '/',
       name: 'Home',
       component: Home
     },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }
   ]
 })
 
